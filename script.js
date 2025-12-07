5591819f4c373232c6365fcca9dadda9bc38abcd
bhuvaneshwari842 <bhuvaneshwari0122@gmail.com>
2025-12-07 16:05:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll(".toggle").forEach(item => {
+    item.addEventListener("click", () => {
+        const content = item.nextElementSibling;
+        content.style.display = content.style.display === "block" ? "none" : "block";
+    });
+});
