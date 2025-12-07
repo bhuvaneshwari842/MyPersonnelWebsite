9c080fa78da0b6f1ba0ca2a1caea345427587902
bhuvaneshwari842 <bhuvaneshwari0122@gmail.com>
2025-12-07 16:39:12 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll(".toggle").forEach(item => {
-    item.addEventListener("click", () => {
-        const content = item.nextElementSibling;
-        content.style.display = content.style.display === "block" ? "none" : "block";
-    });
-});
